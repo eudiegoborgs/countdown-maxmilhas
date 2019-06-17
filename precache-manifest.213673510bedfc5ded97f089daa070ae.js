7b65e53746400eb7750ce7a227c9355fbae11d62
Diego Borges <diego.ferreira@maxmilhas.com.br>
2019-06-17 15:24:17 -0300

    First commit

---
+++ precache-manifest.213673510bedfc5ded97f089daa070ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e1a5e074650dfcfdc13bb8f817da25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8efbbd545e2e079a5997",
+    "url": "/static/css/main.a160c0ce.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "8efbbd545e2e079a5997",
+    "url": "/static/js/main.40a5c54d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e1d54bc29fa7277738f29e7c49aa0490",
+    "url": "/static/media/bg.e1d54bc2.png"
+  },
+  {
+    "revision": "564df194ced969afea69095cd42ea193",
+    "url": "/static/media/maxmilhas.564df194.png"
+  }
+]);
